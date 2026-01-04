ecd23ff5ed98dcf7d1f3e1745bea4caa1b94af04
shanrj17 <shanrj17@gmail.com>
2026-01-04 23:37:20 +0530

    fix(seo): exclude sitemap regex from middleware matcher to fix googlebot fetch

--- middleware.ts
+++ middleware.ts
@@ -64,5 +64,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     // Matcher ignoring `/_next/` and `/api/`
-    matcher: ['/((?!api|_next/static|_next/image|dashboard|favicon.ico|images|assets).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|dashboard|favicon.ico|images|assets|logo.png|globals.css|manifest.ts|next.svg|vercel.svg|robots.txt|sitemap.*).*)'],
 }
